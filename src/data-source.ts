0eec3539fbfc470a8b25f24cd0c3d7fb65243a2c
Agboola Akorede <akoredeagboola23@gmail.com>
2025-03-05 11:55:15 +0100

    fix: meta

--- src/data-source.ts
+++ src/data-source.ts
@@ -2,38 +2,25 @@ import 'reflect-metadata';
 import 'dotenv/config';
 import { DataSource } from 'typeorm';
 
-const isProduction = process.env.NODE_ENV === 'production';
-
-let dataSourceConfig: any;
-
-if (isProduction) {
-  const url = new URL(process.env.DATABASE_URL as string);
-
-  dataSourceConfig = {
-    type: 'postgres',
-    host: url.hostname,
-    port: Number(url.port) || 5432,
-    username: url.username,
-    password: url.password,
-    database: url.pathname.substring(1),
-    synchronize: false,
-    logging: false,
-    ssl: { rejectUnauthorized: false },
-    entities: ['src/entities/**/*.ts'],
-    migrations: ['src/migrations/**/*.ts'],
-  };
-} else {
-  dataSourceConfig = {
-    type: 'postgres',
-    host: process.env.POSTGRESDB_HOST,
-    port: Number(process.env.POSTGRESDB_PORT),
-    username: process.env.POSTGRESDB_USER,
-    password: process.env.POSTGRESDB_PASSWORD,
-    database: process.env.POSTGRESDB_DATABASE,
-    synchronize: false,
-    logging: false,
-    entities: ['src/entities/**/*.ts'],
-    migrations: ['src/migrations/**/*.ts'],
-  };
-}
-export const AppDataSource = new DataSource(dataSourceConfig);
+export const AppDataSource = new DataSource({
+  type: 'postgres',
+  host: process.env.POSTGRESDB_HOST,
+  port: Number(process.env.POSTGRESDB_PORT),
+  username: process.env.POSTGRESDB_USER,
+  password: process.env.POSTGRESDB_PASSWORD,
+  database: process.env.POSTGRESDB_DATABASE,
+  synchronize: false,
+  logging: false,
+  entities: ['src/entities/**/*.ts'],
+  migrations: ['src/migrations/**/*.ts'],
+  subscribers: [],
+  ssl: process.env.POSTGRESDB_SSL === 'true',
+  extra: {
+    ssl:
+      process.env.POSTGRESDB_SSL === 'true'
+        ? {
+            rejectUnauthorized: false,
+          }
+        : undefined,
+  },
+});
